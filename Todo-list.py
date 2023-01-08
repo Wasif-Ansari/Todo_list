627d81244acf52be6b0e903468c9241024cf9f52
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-08 20:21:41 +0530

    giving heading

--- Todo-list.py
+++ Todo-list.py
@@ -6,5 +6,8 @@ root.title("MY TODO LIST")
 root.geometry("400x650+400+100")
 root.resizable(False,False)
 
+heading = Label(root,text="TO-DO-LIST",justify=CENTER,font="50px")
+heading.place(x=125,y=20,height=100,width=150)
+
 
 root.mainloop()
