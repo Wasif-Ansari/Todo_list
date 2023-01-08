33fd32bb600eadb37c14ac3bc4d13dffdb8f381d
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-08 20:22:27 +0530

    making frame

--- Todo-list.py
+++ Todo-list.py
@@ -9,5 +9,7 @@ root.resizable(False,False)
 heading = Label(root,text="TO-DO-LIST",justify=CENTER,font="50px")
 heading.place(x=125,y=20,height=100,width=150)
 
+frame = Frame(root,width=400,height=50, bg="white")
+frame.place(x=0 , y=120)
 
 root.mainloop()
