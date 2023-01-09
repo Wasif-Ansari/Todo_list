cff8c7d83fa807d97c79971576ff7ac4cd1c6ce3
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-09 14:37:42 +0530

    creating listbox for task lists

--- Todo-list.py
+++ Todo-list.py
@@ -20,4 +20,16 @@ task_entry.focus()
 b =Button(frame,text="ADD",font="arial 20 bold", width=6,bg="#5a95ff",fg="#fff",bd=0)
 b.place(x=300 ,y=0)
 
+
+frame1 = Frame(root,bd=3,width=700,height=280,bg="#32405b")
+frame1.pack(pady=(200,20))
+
+listbox = Listbox(frame1,font=("arial",18) , width=40,height=16,bg="#32405b",fg="white", cursor="hand2",selectbackground="#5a95ff")
+listbox.pack(side=LEFT , fill=BOTH, padx=2)
+scrollbar = Scrollbar(frame1)
+scrollbar.pack(side=RIGHT, fill=BOTH)
+
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
+
 root.mainloop()
